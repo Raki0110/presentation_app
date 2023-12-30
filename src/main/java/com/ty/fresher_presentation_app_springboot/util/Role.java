feef3028dfb662d44b39ad901260250f3bcee17d
Raki0110 <rakeshgowdavviet2000@gmail.com>
2023-12-30 12:21:32 +0530

    enumcreated

---
+++ src/main/java/com/ty/fresher_presentation_app_springboot/util/Role.java
@@ -0,0 +1,5 @@
+package com.ty.fresher_presentation_app_springboot.util;
+
+public enum Role {
+	TRAINER,STUDENT
+}
---
+++ src/main/java/com/ty/fresher_presentation_app_springboot/util/Status.java
@@ -0,0 +1,6 @@
+package com.ty.fresher_presentation_app_springboot.util;
+
+public enum Status {
+		ACTIVE,INACTIVE,ASSIGNED,COMPLETED,VOTINGPOLLON
+
+}
