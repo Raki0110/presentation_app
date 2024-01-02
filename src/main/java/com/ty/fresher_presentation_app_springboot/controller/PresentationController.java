67125c94645ae75a3f823c102f0a29534303bfe5
Raki0110 <rakeshgowdavviet2000@gmail.com>
2024-01-02 14:10:23 +0530

    presentation controller

--- src/main/java/com/ty/fresher_presentation_app_springboot/controller/PresentationController.java
+++ src/main/java/com/ty/fresher_presentation_app_springboot/controller/PresentationController.java
@@ -1,5 +1,27 @@
 package com.ty.fresher_presentation_app_springboot.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ty.fresher_presentation_app_springboot.dto.ResponseStucture;
+import com.ty.fresher_presentation_app_springboot.entity.Presentation;
+import com.ty.fresher_presentation_app_springboot.service.PresentationService;
+
+@RestController
 public class PresentationController {
+	
+	@Autowired
+	private PresentationService presentationService; 
+	
+	@PostMapping("/savepresentation")
+	public ResponseEntity<ResponseStucture<Presentation>> savePresentation(@RequestBody Presentation presentation,@PathVariable int id)
+	{
+		return presentationService.savePresentation(presentation, id);
+	}
+	
 
 }
